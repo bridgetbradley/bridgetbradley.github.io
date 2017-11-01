2f83e31c1fc4b9af2bdddea606f625d3bb5637c9
Bridget Bradley <brad0397@algonquinlive.com>
2017-11-01 19:22:27 -0400

    moving files over

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,32 @@ const gulp = require('gulp')
 
 const sass = require('gulp-sass')
 
+const sassPath = 'scss/**/*.scss'
+
+const postcss = require('gulp-postcss')
+
+const autoprefixer = require('autoprefixer')
+
+const cssnano = require('cssnano')
+
+gulp.task('sass', function () {
+  const plugins = [
+    autoprefixer({ browsers: ['last 2 version'] }),
+    cssnano()
+  ]
+
+  return gulp
+
+  .src(sassPath)
+
+  .pipe(sass())
+
+  .pipe(postcss(plugins))
+
+  .pipe(gulp.dest('./css/min'))
+  
+})
+
 gulp.task('sass', function () {
   return gulp
   .src('scss/**/*.scss')
